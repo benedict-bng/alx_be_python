b6037751edc4c6678f4bfc31951daefd3be9d528
Benedict Nderitu <giteebenedict@gmail.com>
2025-08-07 09:52:39 +0300

    Update library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -26,7 +26,6 @@ class PrintBook(Book):
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
-
 class Library:
     def __init__(self):
         self.books = []
@@ -34,9 +33,11 @@ class Library:
     def add_book(self, book):
         self.books.append(book)
 
-    def list_books(self):
+    def display_books(self):
+        print("Library Catalog:")
         for book in self.books:
-            print(book)  # Uses the __str__ method
+            print(book)
+ # Uses the __str__ method
 from library_system import Book, EBook, PrintBook, Library
 
 def main():
